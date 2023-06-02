7162aa31859eabceaa5f22981f90c4423353565e
Selena Chaparro <selewilk95@gmail.com>
2023-06-02 18:59:24 -0300

    subiendo

---
+++ ProyectoUDI/ProyectoUDI/PanelProveedores.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ProyectoUDI
+{
+    public partial class PanelProveedores : Form
+    {
+        public PanelProveedores()
+        {
+            InitializeComponent();
+        }
+    }
+}
