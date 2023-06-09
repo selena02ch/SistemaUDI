5cb1475f5f93e92ad739b23a25e382dc826fd94f
Clarisa Pintos <clap-13@outlook.com>
2023-06-09 19:14:40 -0300

    clase datos

--- ProyectoUDI/ProyectoUDI/ClaseDatos.cs
+++ ProyectoUDI/ProyectoUDI/ClaseDatos.cs
@@ -7,5 +7,81 @@ namespace ProyectoUDI
 {
     class ClaseDatos
     {
+        string nombre, domicilio, usuario, contra;
+        int costo, precioV;
+
+        public int pCosto
+        {
+            set
+            {
+                costo = value;
+            }
+            get
+            {
+                return costo;
+            }
+        }
+
+        public int pPrecioV
+        {
+            set
+            {
+                precioV = value;
+            }
+            get
+            {
+                return precioV;
+            }
+        }
+
+        public string pUsuario
+        {
+            set
+            {
+                usuario = value;
+            }
+            get
+            {
+                return usuario;
+            }
+        }
+
+        public string pContra
+        {
+            set
+            {
+                contra = value;
+            }
+            get
+            {
+                return contra;
+            }
+        }
+
+        public string pNombre
+        {
+            set
+            {
+                nombre = value;
+            }
+            get
+            {
+                return nombre;
+            }
+        }
+
+        public string pDomicilio
+        {
+            set
+            {
+                domicilio = value;
+            }
+            get
+            {
+                return domicilio;
+            }
+        }
+
+    }
     }
 }
