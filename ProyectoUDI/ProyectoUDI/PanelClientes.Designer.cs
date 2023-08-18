9f69928e689f183be937f516402ef948673e623c
sama <samanta.chaparro87@gmail.com>
2023-08-18 19:12:51 -0300

    subiendo

--- ProyectoUDI/ProyectoUDI/PanelClientes.Designer.cs
+++ ProyectoUDI/ProyectoUDI/PanelClientes.Designer.cs
@@ -28,7 +28,71 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lista = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxDomicilio = new System.Windows.Forms.TextBox();
+            this.textBoxNyA = new System.Windows.Forms.TextBox();
+            this.buttonGuardar = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // lista
+            // 
+            this.lista.FormattingEnabled = true;
+            this.lista.Location = new System.Drawing.Point(337, 58);
+            this.lista.Name = "lista";
+            this.lista.Size = new System.Drawing.Size(209, 277);
+            this.lista.TabIndex = 20;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(90, 169);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 19;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(47, 140);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "Domicilio";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(47, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 13);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Nombre y Apellido";
+            // 
+            // textBoxDomicilio
+            // 
+            this.textBoxDomicilio.Location = new System.Drawing.Point(181, 137);
+            this.textBoxDomicilio.Name = "textBoxDomicilio";
+            this.textBoxDomicilio.Size = new System.Drawing.Size(100, 20);
+            this.textBoxDomicilio.TabIndex = 16;
+            // 
+            // textBoxNyA
+            // 
+            this.textBoxNyA.Location = new System.Drawing.Point(181, 69);
+            this.textBoxNyA.Name = "textBoxNyA";
+            this.textBoxNyA.Size = new System.Drawing.Size(100, 20);
+            this.textBoxNyA.TabIndex = 15;
+            // 
+            // buttonGuardar
+            // 
+            this.buttonGuardar.Location = new System.Drawing.Point(103, 296);
+            this.buttonGuardar.Name = "buttonGuardar";
+            this.buttonGuardar.Size = new System.Drawing.Size(75, 23);
+            this.buttonGuardar.TabIndex = 21;
+            this.buttonGuardar.Text = "Guardar";
+            this.buttonGuardar.UseVisualStyleBackColor = true;
             // 
             // PanelClientes
             // 
@@ -36,12 +100,29 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Lavender;
             this.ClientSize = new System.Drawing.Size(592, 393);
+            this.Controls.Add(this.buttonGuardar);
+            this.Controls.Add(this.lista);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxDomicilio);
+            this.Controls.Add(this.textBoxNyA);
             this.Name = "PanelClientes";
             this.Text = "PanelClientes";
+            this.Load += new System.EventHandler(this.PanelClientes_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox lista;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxDomicilio;
+        private System.Windows.Forms.TextBox textBoxNyA;
+        private System.Windows.Forms.Button buttonGuardar;
     }
 }
